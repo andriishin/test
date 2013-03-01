9a207cd647c55fe23893c73e560f1c74cf4698a6
alexander.andriishin <alexander.andriishin@gmail.com>
2013-03-02 01:30:54 +0400

    first commit

---
+++ main/src/com/andriishin/test/HellowWorld.java
@@ -0,0 +1,13 @@
+package com.andriishin.test;
+
+/**
+ * $Revision: $
+ * $Author:  $
+ * $Date:  $
+ */
+public class HellowWorld {
+
+    public static void main(String... args) {
+        System.out.print("hi");
+    }
+}
